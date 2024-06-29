1a84e9ec208723cf63f463b87a1f2aed34294a92
younes oublal <oublal.youness72@gmail.com>
2024-06-29 17:57:30 +0200

    p3

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -21,3 +21,6 @@ async function login() {
     const response = await fetch(url);
     const data = await response.json();
 }
+fetch("http://localhost:5678/api/users/login", { {
+    method: "POST"
+} });
