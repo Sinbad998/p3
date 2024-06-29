8055381357e39aeb2a125224783a3bd82bba4c52
younes oublal <oublal.youness72@gmail.com>
2024-06-29 16:25:46 +0200

    p3

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -7,8 +7,11 @@ form.addEventListener("submit" , (event) =>{
     event.preventDefault()
     let formData = new FormData(form); 
     //console.log(FormData)
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
+    //const email = document.getElementById('email');
+    //const password = document.getElementById('password');
+    const email = formData.getAll('email');
+    const password = formData.getAll('password');
+
     console.log(email.value);
     console.log(password.value);
 })
