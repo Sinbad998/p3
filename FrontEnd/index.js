347bbbae4f56ab9f5fbcce4e1f23e93c1e639b9d
younes oublal <oublal.youness72@gmail.com>
2024-07-05 02:05:55 +0200

    p3

---
+++ FrontEnd/index.js
@@ -0,0 +1,125 @@
+window.addEventListener("load", (event)=> {
+  //si le token du localstorage est vrai alors effacer le menu
+  if (localStorage.token) {
+    //alert("connecter")
+    //const mesProjets = document.querySelector("#contact h2")
+   // mesProjets.textContent = "Mes Projets"
+    //categories.style = "display : none"
+  //const categoryBtn.style = "display : none"
+  //const toutCategoryBtn = document.querySelectorAll(".categoryBtn")
+  document.querySelector(".Menu").innerHTML = "";
+  //document.querySelector("divcontainer").innerHTML = "";
+  //document.q
+  //function cacherMenu(){
+    //toutCategoryBtn.forEach(button =>{
+      //button.style.display = "none"
+    //})
+  //}
+  }else {
+    alert("pas connecter")
+  
+  }
+  }) 
+  //Faites l’appel à l’API avec fetch afin de récupérer dynamiquement les projets de l’architecte.
+  async function getProjectsByArchitect(architectId) {
+  const url = `http://localhost:5678/api/works`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+//Utilisez JavaScript pour ajouter à la galerie les travaux de l’architecte que vous avez récupéré.
+function renderCards(projects) {
+  const galleryContainer = document.querySelector('.gallery')
+  console.log(projects)
+//boucle pour creer une figure et une image pour chaque item dans projects
+  projects.forEach((item, i) => {
+    let figure = document.createElement('figure')
+    , img = document.createElement('img')
+//
+    figure.setAttribute("data-categoryId", item.categoryId)
+    
+    
+    img.src = item.imageUrl
+    //console.log(galleryContainer)
+    figure.append(img)
+    galleryContainer.append(figure)
+    
+    //const reponse = await fetch("")
+  })
+}
+
+// Exemple d'utilisation
+const architectId = 123;
+getProjectsByArchitect(architectId)
+  .then(projects => {
+
+    renderCards(projects)
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+  //Appel à l’API avec fetch afin de récupérer dynamiquement les differentes categories.
+async function categories() {
+  const url = `http://localhost:5678/api/categories`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const div = document.createElement("div")
+  div.classList.add('Menu');
+  const button = document.createElement("button")
+  button.textContent = "Tous"
+  button.addEventListener("click", (event) =>{
+    const worksItem = document.querySelectorAll(".gallery figure")
+//boucle pour que chaque element de workItem est supprime chaque style
+worksItem.forEach(element2 => {
+        element2.style = ""
+      })
+  })
+  div.append(button)
+  const divcontainer = document.querySelector("#portfolio h2")
+  divcontainer.append(div)
+  //console.log(data)
+
+  //boucle pour recuperer chaque id dans le tableau data puis creation de bouton associer
+  data.forEach(element => {
+    const categoryId = element.id;
+    const categoryBtn = document.createElement("button");
+    categoryBtn.textContent = element.name
+    //creation d'evenement click qui selectionne toutes les figure dans .figure
+    categoryBtn.addEventListener("click", (event) => {
+      //alert (event.target)
+      const worksItem = document.querySelectorAll(".gallery figure")
+//boucle pour que chaque element de workItem est supprime chaque style
+worksItem.forEach(element => {
+        element.style = ""
+      })
+//Boucle qui regarde tout les element de workItem
+      worksItem.forEach(element2 => {
+//condition si l'element.id n'est pas égal à element2.dataset.categoryId ceci ne s'affiche pas 
+        if (element.id !== parseInt(element2.dataset.categoryid)) {
+          element2.style = "display : none"
+        }
+      });
+   })
+   
+    div.append(categoryBtn);
+  });
+
+  console.log(button)
+}
+
+
+
+//data.forEach(categoryId => {
+  //const categoryId
+  //})
+  const cats = categories()
+  console.log(cats)
+  
+  
+
+
+
+  
+
+
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -3,24 +3,39 @@ const form = document.getElementById('FormulaireLogin')
 //formData.append("password")
 //console.log(formData)
 
-form.addEventListener("submit" , (event) =>{
+form.addEventListener("submit", (event) => {
     event.preventDefault()
-    let formData = new FormData(form); 
+    let formData = new FormData(form);
     //console.log(FormData)
     //const email = document.getElementById('email');
     //const password = document.getElementById('password');
-    const email = formData.getAll('email');
-    const password = formData.getAll('password');
+    //const email = formData.getAll('email');
+    //const password = formData.getAll('password');
+    const email = form.email.value
+    const password = form.password.value
 
-    console.log(email.value);
-    console.log(password.value);
+    console.log(email);
+    console.log(password);
+
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            email, password
+        })
+    })
+        .then(r => r.json())
+        .then(data => {
+            console.log(data)
+            if (data.token) {
+                localStorage.token = data.token
+                //renvoie vers la page index.html si le token est bon
+                location.href = "index.html"
+            }
+            // si le token n'est pas bon envoi un message d'erreur
+            else alert("erreur: mauvaise authentification")
+        })
+    //.catch(e=>{alert("erreur: mauvaise authentification")})
+
 })
 
-async function login() {
-    const url = `http://localhost:5678/api/users/login`,  methode : "post";
-    const response = await fetch(url);
-    const data = await response.json();
-}
-fetch("http://localhost:5678/api/users/login", { {
-    method: "POST"
-} });
