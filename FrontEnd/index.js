a05502d457a90521465021d8c159e86f37957798
younes oublal <oublal.youness72@gmail.com>
2024-07-12 16:56:28 +0200

    p3

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -26,38 +26,8 @@ window.addEventListener("load", (event) => {
     const ModifBtn = `<div class = "modal1">
     <a href = #modal1 class="js-modal">Modifier</a>
     <aside id="modal1" class="modal" aria-hidden="true" role="dialog" style="display:none"> 
-    <div class ="modal-wrapper">
-    <button class="js-modal-close">Fermer la boite modale</button>
-    <h1>Our love isn't any different from yours, except it's hotter, because I'm involved.</h1>
-    <p>I usually try to keep my sadness pent up inside where it can fester quietly as a mental illness. You lived before you met me?! Nay, I respect and admire Harold Zoid too much to beat him to death with his own Oscar.</p>
-    <p>I wish! It's a nickel. We don't have a brig. I am the man with no name, Zapp Brannigan! <strong> Shinier than yours, meatbag.</strong> <em> I didn't ask for a completely reasonable excuse!</em> I asked you to get busy!</p>
-    <h2>I decline the title of Iron Cook and accept the lesser title of Zinc Saucier, which I just made up. Uhh… also, comes with double prize money.</h2>
-    <p>No, she'll probably make me do it. We'll need to have a look inside you with this camera. We can't compete with Mom! Her company is big and evil! Ours is small and neutral! With gusto.</p>
-    <ol>
-    <li>No! The kind with looting and maybe starting a few fires!</li><li>All I want is to be a monkey of moderate intelligence who wears a suit… that's why I'm transferring to business school!</li><li>Take me to your leader!</li>
-    </ol>
-
-    <input type="text>
-
-    <h3>I'm just glad my fat, ugly mama isn't alive to see this day.</h3>
-    <p>Our love isn't any different from yours, except it's hotter, because I'm involved. Then we'll go with that data file! This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel. That makes me feel angry!</p>
-    <ul>
-    <li>Shut up and take my money!</li><li>Who said that? SURE you can die! You want to die?!</li><li>WINDMILLS DO NOT WORK THAT WAY! GOOD NIGHT!</li>
-    </ul>
-
-    
-    <p>Fry, you can't just sit here in the dark listening to classical music. You can crush me but you can't crush my spirit! OK, if everyone's finished being stupid. Oh, you're a dollar naughtier than most.</p>
-    <p>This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel. That makes me feel angry! When I was first asked to make a film about my nephew, Hubert Farnsworth, I thought "Why should I?" Then later, Leela made the film. But if I did make it, you can bet there would have been more topless women on motorcycles. Roll film!</p>
-    <p>I just told you! You've killed me! Hey! I'm a porno-dealing monster, what do I care what you think? You mean while I'm sleeping in it? No argument here. You wouldn't. Ask anyway!</p>
-    <p>File not found. Oh, how awful. Did he at least die painlessly? …To shreds, you say. Well, how is his wife holding up? …To shreds, you say. I suppose I could part with 'one' and still be feared…</p>
-    <p>I found what I need. And it's not friends, it's things. And until then, I can never die? Oh, I always feared he might run off like this. Why, why, why didn't I break his legs? Is the Space Pope reptilian!?</p>
-    <p>I am Singing Wind, Chief of the Martians. Fry, you can't just sit here in the dark listening to classical music. Guess again. Check it out, y'all. Everyone who was invited is here. Why did you bring us here?</p>
-    <p>No! I want to live! There are still too many things I don't own! Aww, it's true. I've been hiding it for so long. Fry! Quit doing the right thing, you jerk! Man, I'm sore all over. I feel like I just went ten rounds with mighty Thor.</p>
-    <p>Quite possible. We live long and are celebrated poopers. Kif might! Now Fry, it's been a few years since medical school, so remind me. Disemboweling in your species: fatal or non-fatal? Now Fry, it's been a few years since medical school, so remind me. Disemboweling in your species: fatal or non-fatal?</p>
-    <p>Oh right. I forgot about the battle. There's one way and only one way to determine if an animal is intelligent. Dissect its brain! Hey, guess what you're accessories to. Bender, I didn't know you liked cooking. That's so cute.</p>
-    <p>No, she'll probably make me do it. We're rescuing ya. Oh dear! She's stuck in an infinite loop, and he's an idiot! Well, that's love for you. You can see how I lived before I met you. Now, now. Perfectly symmetrical violence never solved anything.</p>
-    <p>Oh, I think we should just stay friends. Good news, everyone! I've taught the toaster to feel love! They're like sex, except I'm having them! Fetal stemcells, aren't those controversial?</p>
-    <a href="#">test667</a>
+    <div class ="modal-wrapper js-modal-stop">
+    <h1>Galerie photo</h1>
     </div>
     </aside>`
     document.querySelector("#portfolio h2").insertAdjacentHTML("afterend", ModifBtn)
@@ -70,8 +40,12 @@ window.addEventListener("load", (event) => {
       target.setAttribute("aria-hidden", false)
       modal = target
       modal.addEventListener("click" , closemodal)
-      modal.querySelector("js-modal-close").addEventListener("click", closemodal)
-      modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
+      //modal.querySelector("js-modal-close").addEventListener("click", closemodal)
+      //const modalBtn = document.querySelector("js-modal-close");
+      modal.addEventListener('click' , ()=>{
+        
+      });
+      //modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
     }
 
   // Fonction pour fermer la modale et suppression des evenements
@@ -82,13 +56,10 @@ window.addEventListener("load", (event) => {
       modal.setAttribute("aria-hidden", true)
       modal.removeEventListener("click" , closemodal)
       modal = null
-      modal.querySelector("js-modal-close").removeEventListener("click", closemodal)
-      modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)
-      
     }
 
     const stopPropagation = function (e) {
-      e;stopPropagation()
+      e.stopPropagation()
     }
 
 
@@ -96,6 +67,14 @@ window.addEventListener("load", (event) => {
       a.addEventListener("click", ouvrirmodal)
 
     })
+
+    window.addEventListener("keydown", function(e){
+      if (e.key ==="Escape"){
+        closemodal(e)
+      }
+    })
+
+    
 
 
 
