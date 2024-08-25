3785aa3b048365421709b8c0f1089fc81843434e
younes oublal <oublal.youness72@gmail.com>
2024-08-25 17:37:27 +0200

    p3

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -5,8 +5,8 @@
 window.addEventListener("load", (event) => {
   //si le token du localstorage est vrai alors effacer le menu
   if (localStorage.token) {
-
-
+    
+    
     // Fonction pour afficher la modale et ajout d'evenement
     const ouvrirmodal = function (e) {
       e.preventDefault()
@@ -16,7 +16,7 @@ window.addEventListener("load", (event) => {
       modal = target
       console.log(modal)
       // modal.addEventListener("click", closemodal)
-
+      
       //modal.querySelector("js-modal-close").addEventListener("click", closemodal)
       //const modalBtn = document.querySelector("js-modal-close");
       modal.addEventListener('click', async () => {
@@ -29,7 +29,7 @@ window.addEventListener("load", (event) => {
       });
       //modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
     }
-
+    
     // Fonction pour fermer la modale et suppression des evenements
     const closemodal = function (e) {
       if (modal === null) return
@@ -39,15 +39,15 @@ window.addEventListener("load", (event) => {
       modal.removeEventListener("click", closemodal)
       modal = null
     }
-
+    
     // afficher le bandeau mode edition
     const template = `<div class="modeedition">
 		<img src="/Vector.png" alt="">
 		<span>mode édition</span>
 	  </div>`
     document.body.insertAdjacentHTML("afterbegin", template)
-
-
+    
+    
     //changer le bouton en logout
     const BtnLogout = document.querySelector("li:nth-child(3)");
     BtnLogout.textContent = "Logout";
@@ -56,16 +56,17 @@ window.addEventListener("load", (event) => {
       localStorage.clear()
       location.href = ""
     })
-
+    
   } else {
     alert("pas connecter")
     categories()
     document.querySelector("li:nth-child(3)").addEventListener("click", () => {
       location.href = "login.html"
     })
-
+    
+    modal_template.style.display = "none"
     // pour la fonctionnalites du logout pour afficher les filtres
-
+    
   }
 })
 //Faites l’appel à l’API avec fetch afin de récupérer dynamiquement les projets de l’architecte.
@@ -82,16 +83,16 @@ function renderCards(projects) {
   //boucle pour creer une figure et une image pour chaque item dans projects
   projects.forEach((item, i) => {
     let figure = document.createElement('figure')
-      , img = document.createElement('img')
+    , img = document.createElement('img')
     //
     figure.setAttribute("data-categoryId", item.categoryId)
-
-
+    
+    
     img.src = item.imageUrl
     //console.log(galleryContainer)
     figure.append(img)
     galleryContainer.append(figure)
-
+    
     //const reponse = await fetch("")
   })
 }
@@ -99,14 +100,14 @@ function renderCards(projects) {
 // Exemple d'utilisation
 const architectId = 123;
 getProjectsByArchitect(architectId)
-  .then(projects => {
-
-    renderCards(projects)
-    renderModalCards(projects)
-  })
-  .catch(error => {
-    console.log(error);
-  });
+.then(projects => {
+  
+  renderCards(projects)
+  renderModalCards(projects)
+})
+.catch(error => {
+  console.log(error);
+});
 
 
 
@@ -130,7 +131,7 @@ async function categories() {
   const divcontainer = document.querySelector("#portfolio h2")
   divcontainer.append(div)
   //console.log(data)
-
+  
   //boucle pour recuperer chaque id dans le tableau data puis creation de bouton associer
   data.forEach(element => {
     const categoryId = element.id;
@@ -152,10 +153,10 @@ async function categories() {
         }
       });
     })
-
+    
     div.append(categoryBtn);
   });
-
+  
   console.log(button)
 }
 
@@ -181,36 +182,36 @@ modal.addEventListener('click', e => [
 
 function renderModalCards(projects) {
   const imageTravauxWrapper = document.querySelector(".modal-wrapper div");
-
+  
   projects.forEach((item, i) => {
     let figure = document.createElement('figure')
     img = document.createElement('img')
     const button = document.createElement('button');
     button.className = 'fa-solid fa-trash-can'
     button.classList.add('trash')
-
+    
     button.addEventListener("click", () => {
       fetch("http://localhost:5678/api/works/" + item.id, {
         method: "DELETE",
         headers: { "Authorization": "Bearer " + localStorage.token },
       })
-
-        .then(response => {
-          if (response.ok) {
-            console.log("Travaux supprimer correctement");
-          } else {
-            console.error("Erreur travaux non supprimer", response.status);
-          }
-        })
-        .catch(error => {
-          console.error("Erreur envoi de la requete DELETE", error);
-        });
+      
+      .then(response => {
+        if (response.ok) {
+          console.log("Travaux supprimer correctement");
+        } else {
+          console.error("Erreur travaux non supprimer", response.status);
+        }
+      })
+      .catch(error => {
+        console.error("Erreur envoi de la requete DELETE", error);
+      });
     })
-
+    
     figure.setAttribute("data-categoryId", item.categoryId)
-
+    
     img.src = item.imageUrl
-
+    
     figure.append(button);
     figure.append(img)
     imageTravauxWrapper.append(figure)
@@ -228,8 +229,8 @@ ajoutBtn.addEventListener("click", (item,) => {
   ajoutBtn.textContent = "Valider";
   ajoutBtn.style = "background-color : #A7A7A7 ; border : white"
   ajoutBtn.style = 'display : none';
-
-
+  
+  
   
   const gallerieContainer = document.querySelector('.modal-galerie');
   gallerieContainer.innerHTML= "";
@@ -238,47 +239,69 @@ ajoutBtn.addEventListener("click", (item,) => {
   gallerieContainer.classList.add("nouveau")
   //document.querySelector(".Ajout").addEventListener("click", (item, i) => {
     event.preventDefault();
-    const titleH1 = document.querySelector('.modal-wrapper h1');
-    titleH1.textContent = "Ajout photo";
-    modal.classList.add("on")
+  const titleH1 = document.querySelector('.modal-wrapper h1');
+  titleH1.textContent = "Ajout photo";
+  modal.classList.add("on")
+  
+  const btnValider = document.createElement('button');
+  btnValider.textContent = "Valider" 
+  btnValider.classList.add("Valider")
+  btnValider.addEventListener("click", ()=>{
+    console.log("j'ai cliquer");
+    function champsValides(){
+      validerTitle()
+      validerCategories()
+    }
     
-    const btnValider = document.createElement('button');
-    btnValider.textContent = "Valider" 
-    btnValider.classList.add("Valider")
-    ajoutBtn.insertAdjacentElement("afterend", btnValider);
-    btnValider.addEventListener("click", ()=>{
+    function validerTitle(title) {
+      let titleRegExp = new RegExp("[a-z0-9._-]")
+      if (!titleRegExp.test(title)) {
+        throw new Error("Le titre n'est pas valide.")
+      }
+    }
 
-    })
+    function validerCategories(categories){
+      if(!categoriesSelect.value)
+        throw new Error("la category n'as pas été selectioneer")
+    }
 
-    const flecheArriere = document.createElement("div");
-    flecheArriere.classList.add('fleche')
-    flecheArriere.innerHTML='<i class="fa-solid fa-arrow-left"></i>'
-    flecheArriere.addEventListener("click", ()=>{
-      
-      const modal2 = document.getElementById('menu')
-      //modal2.innerHTML=""
-      modal2.remove()
-      document.querySelector(".nouveau form").remove()
-      document.querySelector(".nouveau").classList.remove("nouveau")
-      ajoutBtn.textContent = "Ajouter une photo";
-      titleH1.innerHTML = "Galerie photo"
-      //ajoutBtn.style = ""
-    })
+    if (!champsValides()) {
+      console.log('Veuillez remplir tous les champs correctement.');
+      return;
+    }
+    else{
 
-    const croix = document.createElement("div");
-    croix.classList.add('croix')
-    croix.innerHTML='<i class="fa-solid fa-x"></i>'
-    croix.addEventListener("click", ()=>{
-      const modalFermer = document.getElementById('modal');
-      modalFermer.classList.remove("on")
+    }
+  })
+  
+  const flecheArriere = document.createElement("div");
+  flecheArriere.classList.add('fleche')
+  flecheArriere.innerHTML='<i class="fa-solid fa-arrow-left"></i>'
+  flecheArriere.addEventListener("click", ()=>{
+    
+    const modal2 = document.getElementById('menu')
+    modal2.remove()
+    document.querySelector(".nouveau form").remove()
+    document.querySelector(".nouveau").classList.remove("nouveau")
+    ajoutBtn.textContent = "Ajouter une photo";
+    titleH1.innerHTML = "Galerie photo"
 
-      flecheArriere.click()
-    })
-
-
-    const rechercheImages = document.createElement('div')
-    //rechercheImages.insertAdjacentElement("beforebegin", gallerieContainer)
-    const ajoutPhotoTemplate = `
+  })
+  
+  const croix = document.createElement("div");
+  croix.classList.add('croix')
+  croix.innerHTML='<i class="fa-solid fa-x"></i>'
+  croix.addEventListener("click", ()=>{
+    const modalFermer = document.getElementById('modal');
+    modalFermer.classList.remove("on")
+    
+    flecheArriere.click()
+  })
+  
+  
+  const rechercheImages = document.createElement('div')
+  //rechercheImages.insertAdjacentElement("beforebegin", gallerieContainer)
+  const ajoutPhotoTemplate = `
     <label id="menu"> 
       <img src="assets/icons/picture-svgrepo-com 1.png" alt="">
       <input type="file" id="file-input" accept="image/jpeg, image/png, image/4mo , image/jpg"/>
@@ -287,54 +310,53 @@ ajoutBtn.addEventListener("click", (item,) => {
         jpg, png : 4mo max
       </span>
     </label>`
+  
+  document.querySelector('.modal-wrapper h1').insertAdjacentHTML("afterend" ,ajoutPhotoTemplate)
+  
+  const fileInput =  document.getElementById('file-input');
+  fileInput.addEventListener('change', previewFile);
+  
+  function previewFile () {
+    // regex pour mettre en parametres jpeg et png
+    const fileRegex = /\.(jpeg|jpg|png)$/i;
     
-    document.querySelector('.modal-wrapper h1').insertAdjacentHTML("afterend" ,ajoutPhotoTemplate)
-
-    const fileInput =  document.getElementById('file-input');
-    fileInput.addEventListener('change', previewFile);
-
-    function previewFile () {
-// regex pour mettre en parametres jpeg et png
-      const fileRegex = /\.(jpeg|jpg|png)$/i;
-
-      if (this.files.length === 0  || !fileRegex.
-        test(this.files[0].name)) {
+    if (this.files.length === 0  || !fileRegex.
+      test(this.files[0].name)) {
         return;
       }
-
+      
       const file = this.files[0];
-
+      
       const file_reader = new FileReader();
-
+      
       file_reader.readAsDataURL(file);
-
+      
       file_reader.addEventListener('load', (event) =>
         displayImage(event,file));
-        const divAjout = document.querySelector('.divAjout');
-        divAjout.style = 'display : none';
-        const spanAjout = document.querySelector('.spanAjout')
-        spanAjout.style = 'display:none'
+      const divAjout = document.querySelector('.divAjout');
+      divAjout.style = 'display : none';
+      const spanAjout = document.querySelector('.spanAjout')
+      spanAjout.style = 'display:none'
       
     } 
-
+    
     function displayImage(event, file) {
       const figureElement = document.createElement('figure');
       figureElement.id = "image_selectionner";
-
+      
       const imageElement = document.createElement('img');
       imageElement.src = event.target.result;
-
+      
       figureElement.appendChild(imageElement);
       document.body.querySelector('label#menu').classList.add("loaded")
       document.body.querySelector('label#menu img').src = event.target.result;
       
-
+      
     }
-    //document.body.querySelector('label #menu').appendChild(figureElement);
-
+    
     const image = document.createElement('img');
     rechercheImages.appendChild(image);
-
+    
     const btnContainer = document.querySelector('.modal-wrapper')
     btnContainer.insertAdjacentElement('afterend', rechercheImages);
     //const modalForm = document.createElement('div')
@@ -343,14 +365,15 @@ ajoutBtn.addEventListener("click", (item,) => {
     //document.querySelector('.modal-wrapper label').insertAdjacentHTML("afterend" ,modalForm)
     
     const form = document.createElement('form');
-
+    
     const titleLabel = document.createElement('label');
     titleLabel.textContent = 'Titre';
     form.appendChild(titleLabel);
-
+    
     const titleInput = document.createElement('input');
     titleInput.type = 'text';
     titleInput.placeholder = '';
+    titleInput.name = 'title';
     form.appendChild(titleInput);
     
     const categoriesLabel = document.createElement('label');
@@ -360,48 +383,97 @@ ajoutBtn.addEventListener("click", (item,) => {
     
     const categoriesSelect = document.createElement('select');
     categoriesSelect.id = 'categoriesSelect';
-
-
+    categoriesSelect.name = 'categories';
+    
+    
     
     
     form.appendChild(categoriesLabel);
     form.appendChild(categoriesSelect);
+    form.appendChild(btnValider);
+    
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      let formData = new FormData(form);
+
+      let title = form.title.value;
+      let categories = form.categories.value;
+      let imageForm = form.image;
+      
+     
+      console.log(categories)
+      console.log(title)
+
+      const bodyContent = JSON.stringify({
+        image,title,categories
+      })
+      
+      fetch("http://localhost:5678/api/works/", {
+        method: "POST",
+        headers: {"Authorization": "Bearer " +  localStorage.getItem("token")  },
+        body: bodyContent
+        })
+        .then(response => response.json())
+        .then(data =>{
+          console.log(data)
+        
+          const imageUrl = imageForm;
+          console.log(imageUrl)
+          const galleryContainer = document.querySelector('.gallery');
+  
+          const figure = document.createElement('figure');
+          const image = document.createElement('img');
+          image.src = imageUrl;
+
+  
+          figure.appendChild(image);
+          galleryContainer.appendChild(figure);
+
+          alert('Le nouvel travail a été ajouté avec succès.')
+        })
+        .catch(error =>{
+          console.eror ("Une erreur c'est produite")
+          alert('Une erreur cest produite')
+        })
+      
+      
+    });
     
     gallerieContainer.appendChild(form)
     btnContainer.appendChild(rechercheImages)
     btnContainer.appendChild(flecheArriere)
     btnContainer.appendChild(croix)
-
-  
-
-  async function fillFormSelect(item,i){
-    const url = `http://localhost:5678/api/categories`;
-    const response = await fetch(url);
-    const data = await response.json();
-    const defaultOption = `<option value="0"></option>`
-    console.log(categoriesSelect)
-    categoriesSelect.innerHTML = defaultOption;
-
-    data.forEach(category => {
-      console.log(category)
-      const option = document.createElement('option');
-      option.value = category.id;
-      option.textContent = category.name;
-      categoriesSelect.appendChild(option);
-    })
-  }
-  fillFormSelect()
-
     
-})
-
-const buttonContainer = document.querySelector('.modal-wrapper');
-buttonContainer.appendChild(ajoutBtn);
-//console.log(buttonContainer)
-
-
+    
+    
+    async function fillFormSelect(item,i){
+      const url = `http://localhost:5678/api/categories`;
+      const response = await fetch(url);
+      const data = await response.json();
+      const defaultOption = `<option value="0"></option>`
+      console.log(categoriesSelect)
+      categoriesSelect.innerHTML = defaultOption;
+      
+      data.forEach(category => {
+        console.log(category)
+        const option = document.createElement('option');
+        option.value = category.id;
+        option.textContent = category.name;
+        categoriesSelect.appendChild(option);
+      })
+    }
+    fillFormSelect()
+    
+    
+  })
   
-
+  const buttonContainer = document.querySelector('.modal-wrapper');
+  buttonContainer.appendChild(ajoutBtn);
+  //console.log(buttonContainer)
+  
+  
+  
+  
   const modal_template2 = `
   <form id="FormulaireAjout">
   <label for="username">Titre :</label>
@@ -414,83 +486,29 @@ buttonContainer.appendChild(ajoutBtn);
   `
   const modalContainer = document.createElement('div');
   modalContainer.insertAdjacentHTML('beforeend', modal_template2);
-//console.log(modalContainer)
+  //console.log(modalContainer)
   document.body.appendChild(modalContainer);
-
-// Gestion de l'événement Submit sur le formulaire
+  
+  // Gestion de l'événement Submit sur le formulaire
   let form = document.getElementById('FormulaireAjout');
   const btnValider = document.querySelector('.Valider');
-  form.appendChild(btnValider)
+  //form.appendChild(btnValider)
   console.log(btnValider)
   
- // console.log(form)
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let formData = new FormData(form);
-    //let baliseTitle = document.getElementById('title')
-    //const baliseCategories = document.getElementById('categories')
-    const title = form.title.value;
-    let categories = form.categories.value;
-    let imageForm = form.image;
-    
-    console.log(categories)
-    console.log(title)
+  // console.log(form)
   
-    
-
-    fetch("http://localhost:5678/api/works/", {
-      method: "POST",
-      headers: {"Authorization": "Bearer " +  localStorage.token  },
-      body: ({
-        image,title,category
-      })
-      .then(response => response.json())
-      .then(data =>{
-        if(data.success) {
-          console.log("Creation effectuer")
-        } else {
-          console.error("Erreur")
-        }
-      })
-      .catch(error =>{
-        console.eror ("erreur")
-      })
-    })
-
-  });
-
-
- //btnValider = document.querySelector('.Valider')
- console.log(btnValider)
- btnValider.addEventListener("click", () =>{
-  console.log("j'ai cliquer");
-   if (!champsValides()) {
-     afficherMessageErreur('Veuillez remplir tous les champs correctement.');
-     return;
-   }
-   function champsValides(){
-     validerEmail()
-   }
-
-   function validerTitle(title) {
-     let titleRegExp = new RegExp("[a-z0-9._-]")
-     if (!titleRegExp.test(title)) {
-         throw new Error("Le titre n'est pas valide.")
-     }
- }
- 
- })
-
-
-
+  //btnValider = document.querySelector('.Valider')
+  console.log(btnValider)
+  
+  
   //data.forEach(categoryId => {
-  //const categoryId
+    //const categoryId
   //})
   //const cats = categories()
   //console.log(cats)
   
-// faut que j'essaie de supprimer ou mettre en commentaire ce que je'ai fais pour essayer de tout simplement reprendre la modaletemplate et la modifier comme par exemple gallery-wrapper je met form titre aisni de suite
-// c'est chiant mais faisable faut juste que mon cerveau soit organiser ainsi que mes idees pour que j'y arrive parce que en faite j'ai trop d'idee dans la tete 
-//  bref ce soir objectif faire apparaitre l'espace vide avec un titre prenom et l'espace vide avec les categories 
-// normalement ce que j'ai fais ça marche mais ça s'ouvre pas dans la modale 
-//document.body.insertAdjacentHTML("beforeend", modal_template2);
+  // faut que j'essaie de supprimer ou mettre en commentaire ce que je'ai fais pour essayer de tout simplement reprendre la modaletemplate et la modifier comme par exemple gallery-wrapper je met form titre aisni de suite
+  // c'est chiant mais faisable faut juste que mon cerveau soit organiser ainsi que mes idees pour que j'y arrive parce que en faite j'ai trop d'idee dans la tete 
+  //  bref ce soir objectif faire apparaitre l'espace vide avec un titre prenom et l'espace vide avec les categories 
+  // normalement ce que j'ai fais ça marche mais ça s'ouvre pas dans la modale 
+  //document.body.insertAdjacentHTML("beforeend", modal_template2);
